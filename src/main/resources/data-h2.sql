d4bc5645b45ca750a5d99412c7aeedb2aa684408
BEDongryeol <ehdfuf53@gmail.com>
2021-12-09 14:03:37 +0900

    feat: Add data-h2.sql
    Added for DB initialization

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,6 +1,12 @@
-INSERT INTO products(title, total_investing_amount, started_at, finished_at)
+INSERT INTO products(title, total_invest_amount, started_at, finished_at)
 VALUES ('You can be elon musk', 100000000, CURDATE() - 1, CURDATE() + 1),
        ('You also can be bill gates', 500000000, CURDATE() + 1 , CURDATE() + 2),
        ('or jobs?', 300000000, CURDATE() + 2, CURDATE() + 3),
        ('TOBE-RICH of Warren Buffett', 600000000, CURDATE() - 2, CURDATE() + 2)
 ;
+
+INSERT INTO USER(ID, HOLDING_AMOUNT, INVESTED_AMOUNT, INVESTED_COUNT, STATUS)
+VALUES (1, 3000000, 0, 0, 'NONE'),
+       (2, 3000000, 0, 0, 'NONE')
+;
+
--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -4,7 +4,9 @@ CREATE TABLE products
 (
     id                     bigint       NOT NULL AUTO_INCREMENT, --id
     title                  varchar(100) NOT NULL,                --상품명
-    total_investing_amount bigint       NOT NULL,                --총 투자 모집금액
+    total_invest_amount bigint       NOT NULL,                --총 투자 모집금액
+    invested_count          bigint       DEFAULT 0,               --투자자 수
+    invested_amount         bigint       DEFAULT 0,               --현재까지 투자 금액
     started_at             datetime     NOT NULL,                --투자시작일시
     finished_at            datetime     NOT NULL,                --투자종료일시
     PRIMARY KEY (id)
